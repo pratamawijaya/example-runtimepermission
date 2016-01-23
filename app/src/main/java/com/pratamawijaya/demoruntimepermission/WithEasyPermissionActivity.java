38319fc217b4d6601c6584d167e338c1c84dcc8f
pratamawijaya <set.mnemonix@gmail.com>
2016-01-23 18:34:23 +0700

    add logic

--- app/src/main/java/com/pratamawijaya/demoruntimepermission/WithEasyPermissionActivity.java
+++ app/src/main/java/com/pratamawijaya/demoruntimepermission/WithEasyPermissionActivity.java
@@ -1,16 +1,21 @@
 package com.pratamawijaya.demoruntimepermission;
 
 import android.Manifest;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
+import com.google.android.gms.location.LocationRequest;
 import com.pratamawijaya.demoruntimepermission.easypermission.AfterPermissionGranted;
 import com.pratamawijaya.demoruntimepermission.easypermission.EasyPermissions;
 import java.util.List;
+import pl.charmas.android.reactivelocation.ReactiveLocationProvider;
+import rx.functions.Action1;
 
 public class WithEasyPermissionActivity extends AppCompatActivity
     implements EasyPermissions.PermissionCallbacks {
@@ -35,13 +40,25 @@ public class WithEasyPermissionActivity extends AppCompatActivity
    * get access location
    */
   @AfterPermissionGranted(RP_ACCESS_LOCATION) private void accessLocation() {
-    String perm[] = { Manifest.permission.ACCESS_FINE_LOCATION };
-    if (EasyPermissions.hasPermissions(this, perm)) {
-      Toast.makeText(WithEasyPermissionActivity.this, "Yay, has location permission",
-          Toast.LENGTH_SHORT).show();
+
+    String[] permission = { Manifest.permission.ACCESS_FINE_LOCATION };
+    if (EasyPermissions.hasPermissions(this, permission)) {
+      LocationRequest request = LocationRequest.create() //standard GMS LocationRequest
+          .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY).setNumUpdates(5).setInterval(100);
+
+      ReactiveLocationProvider locationProvider = new ReactiveLocationProvider(this);
+
+      locationProvider.getUpdatedLocation(request).subscribe(new Action1<Location>() {
+        @Override public void call(Location location) {
+          Log.d("tag", "location : " + location.getLatitude());
+          Toast.makeText(WithEasyPermissionActivity.this, "" + location.getLatitude(),
+              Toast.LENGTH_SHORT).show();
+        }
+      });
     } else {
+      Log.d("tag", "need permission");
       EasyPermissions.requestPermissions(this, "Access Location dibutuhkan", RP_ACCESS_LOCATION,
-          perm);
+          permission);
     }
   }
 
@@ -52,10 +69,10 @@ public class WithEasyPermissionActivity extends AppCompatActivity
   }
 
   @Override public void onPermissionsGranted(List<String> perms) {
-
+    Log.d("tag", "permission granted");
   }
 
   @Override public void onPermissionsDenied(List<String> perms) {
-
+    Log.d("tag", "permission denied");
   }
 }
--- app/src/main/java/com/pratamawijaya/demoruntimepermission/WithoutEasyPermissionActivity.java
+++ app/src/main/java/com/pratamawijaya/demoruntimepermission/WithoutEasyPermissionActivity.java
@@ -3,6 +3,7 @@ package com.pratamawijaya.demoruntimepermission;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
@@ -10,8 +11,12 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
+import com.google.android.gms.location.LocationRequest;
+import pl.charmas.android.reactivelocation.ReactiveLocationProvider;
+import rx.functions.Action1;
 
 public class WithoutEasyPermissionActivity extends AppCompatActivity {
 
@@ -46,6 +51,10 @@ public class WithoutEasyPermissionActivity extends AppCompatActivity {
           Manifest.permission.ACCESS_FINE_LOCATION)) {
         Toast.makeText(WithoutEasyPermissionActivity.this, "Access Location dibutuhkan",
             Toast.LENGTH_SHORT).show();
+
+        String[] perm = { Manifest.permission.ACCESS_FINE_LOCATION };
+        ActivityCompat.requestPermissions(WithoutEasyPermissionActivity.this, perm,
+            RP_ACCESS_LOCATION);
       } else {
         // request permission untuk access fine location
         String[] perm = { Manifest.permission.ACCESS_FINE_LOCATION };
@@ -69,6 +78,8 @@ public class WithoutEasyPermissionActivity extends AppCompatActivity {
           // access location didapatkan
           Toast.makeText(WithoutEasyPermissionActivity.this, "Yay, has location permission",
               Toast.LENGTH_SHORT).show();
+
+          doSomething();
         } else {
           // access location ditolak user
           Toast.makeText(WithoutEasyPermissionActivity.this, "permission ditolak user",
@@ -76,5 +87,20 @@ public class WithoutEasyPermissionActivity extends AppCompatActivity {
         }
         return;
     }
+  }
+
+  private void doSomething() {
+    LocationRequest request = LocationRequest.create() //standard GMS LocationRequest
+        .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY).setNumUpdates(5).setInterval(100);
+
+    ReactiveLocationProvider locationProvider = new ReactiveLocationProvider(this);
+
+    locationProvider.getUpdatedLocation(request).subscribe(new Action1<Location>() {
+      @Override public void call(Location location) {
+        Log.d("tag", "location : " + location.getLatitude());
+        Toast.makeText(WithoutEasyPermissionActivity.this, "" + location.getLatitude(),
+            Toast.LENGTH_SHORT).show();
+      }
+    });
   }
 }
